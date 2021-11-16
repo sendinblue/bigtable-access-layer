15c45167eb66db3c564d2e0979b8db23d8200f69
Alex Debril <alexdebril@users.noreply.github.com>
2021-11-16 16:28:07 +0100

    Remove useless mutation

--- example/main.go
+++ example/main.go
@@ -60,47 +60,40 @@ func aggregate(ctx context.Context, client *bigtable.Client, out io.Writer, tabl
 	}
 	mapper := mapping.NewMapper(jsonMapping)
 	for fam, items := range row {
-		_,_ = fmt.Fprintf(out, "family: %s\n", fam)
+		_, _ = fmt.Fprintf(out, "family: %s\n", fam)
 		columns, events := mapper.GetMappedEvents(items, false)
-		_,_ = fmt.Fprintf(out, "mapped columns: %+v\n", columns)
+		_, _ = fmt.Fprintf(out, "mapped columns: %+v\n", columns)
 		cnt := aggregation.NewCount("count")
 		grpDeviceEvent := aggregation.GroupByAggregate(events, cnt.Compute, "device_type", "event_type")
 		for _, result := range grpDeviceEvent {
-            _,_ = fmt.Fprintf(out, "device: %v, event: %v , count: %v\n", result.Cells["device_type"], result.Cells["event_type"], result.Cells["count"])
-        }
+			_, _ = fmt.Fprintf(out, "device: %v, event: %v , count: %v\n", result.Cells["device_type"], result.Cells["event_type"], result.Cells["count"])
+		}
 		grpEvent := aggregation.GroupByAggregate(events, cnt.Compute, "event_type")
 		for _, result := range grpEvent {
-			_,_ = fmt.Fprintf(out, "event: %v , count: %v\n", result.Cells["event_type"], result.Cells["count"])
+			_, _ = fmt.Fprintf(out, "event: %v , count: %v\n", result.Cells["event_type"], result.Cells["count"])
 		}
 	}
 	return nil
 }
 
 func fillTable(ctx context.Context, client *bigtable.Client, table string) error {
-    tbl := client.Open(table)
+	tbl := client.Open(table)
 	numContacts := 10
 	for i := 0; i < numContacts; i++ {
 		row := fmt.Sprintf("contact-%d", i+1)
 		mutations := generateMutations(100)
 		for _, m := range mutations {
-            if err := tbl.Apply(ctx, row, m); err != nil {
-                return err
-            }
-        }
+			if err := tbl.Apply(ctx, row, m); err != nil {
+				return err
+			}
+		}
 	}
-
-    mut := bigtable.NewMutation()
-    mut.Set("front", "col", bigtable.Now(), []byte("value 3"))
-    err := tbl.Apply(ctx, "row", mut)
-    if err != nil {
-        return err
-    }
-    return nil
+	return nil
 }
 
 func generateMutations(numEvents int) []*bigtable.Mutation {
 	var data []*bigtable.Mutation
-    for i := 0; i < numEvents; i++ {
+	for i := 0; i < numEvents; i++ {
 		mod := i % 11
 		mut := bigtable.NewMutation()
 		t := bigtable.Time(time.Now().Add(-time.Duration(i) * time.Minute))
@@ -109,12 +102,12 @@ func generateMutations(numEvents int) []*bigtable.Mutation {
 		case 1, 2:
 			mut.Set("front", "2", t, []byte("1"))
 		case 3:
-            mut.Set("front", "3", t, []byte("1"))
+			mut.Set("front", "3", t, []byte("1"))
 		default:
 			mut.Set("front", "1", t, []byte("1"))
 		}
-		mut.Set("front", "d", t, []byte(fmt.Sprintf("%d", 1 + (i % 2))))
+		mut.Set("front", "d", t, []byte(fmt.Sprintf("%d", 1+(i%2))))
 		data = append(data, mut)
-    }
-    return data
+	}
+	return data
 }
