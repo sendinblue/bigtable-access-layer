db3da6f7358a317111c6dd53a38f141c35b0a271
ced42 <17880812+ced42@users.noreply.github.com>
2024-04-08 12:12:01 +0200

    fix error: concurrent map read and map write

--- mapping/to_event.go
+++ mapping/to_event.go
@@ -1,5 +1,7 @@
 package mapping
 
+import "sync"
+
 // turnToShortColumn is a default seeker that simply translates the column's name if a match exists.
 func seekFromShortColumn(m *Mapping, column string, value string) (bool, string, string) {
 	r, ok := m.Raws[column]
@@ -41,19 +43,17 @@ type cacheEntry struct {
 
 // reverseTransformer is to use for data stored in a "reversed way" meaning that the column qualifier is the actual data and is a kind of enum value
 type reverseSeeker struct {
-	cache map[string]*cacheEntry
+	cache sync.Map
 }
 
 func newReverseSeeker() *reverseSeeker {
-	return &reverseSeeker{
-		cache: make(map[string]*cacheEntry),
-	}
+	return &reverseSeeker{}
 }
 
 // seekFromCache returns the column and value from the cache if it exists.
 func (c *reverseSeeker) seekFromCache(_ *Mapping, column string, _ string) (bool, string, string) {
-	if entry, ok := c.cache[column]; ok {
-		return true, entry.col, entry.value
+	if entry, ok := c.cache.Load(column); ok {
+		return true, entry.(cacheEntry).col, entry.(cacheEntry).value
 	}
 	return false, "", ""
 }
@@ -63,7 +63,7 @@ func (c *reverseSeeker) seekFromMapping(m *Mapping, column string, _ string) (bo
 	for _, ma := range m.Reversed {
 		for short, val := range ma.Values {
 			if column == short {
-				c.cache[column] = &cacheEntry{col: ma.Name, value: val}
+				c.cache.Store(column, cacheEntry{col: ma.Name, value: val})
 				return true, ma.Name, val
 			}
 		}
